a0a5e5acbd74ea66744f38d3e7e1002547f852a8
sunnyverma86 <61960321+sunnyverma86@users.noreply.github.com>
2024-04-16 19:06:37 +0530

    second commit

---
+++ First.java
@@ -0,0 +1,16 @@
+import java.time.Instant;
+public class First
+{
+	public static void main(String...r)
+	{
+		System.out.println("hello World");
+		System.out.println(Instant.now());
+		System.out.println("no where home jenkins");
+
+		System.out.println("no where home jenkins");
+		System.out.println("no where home jenkins");
+		System.out.println("no where home jenkins");
+		System.out.println("no where home jenkins");
+	}
+}
+
